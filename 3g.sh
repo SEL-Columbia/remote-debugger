78a9c27eecc9692b02f6714411d94af2b665b210
Chris Tan <csytan@gmail.com>
2016-02-10 17:15:27 -0500

    Changes for running on desktop
     + new podsystems sim defaults

--- 3g.sh
+++ 3g.sh
@@ -4,6 +4,12 @@
 
 echo 'starting umtskeeper'
 
+USBINTERFACE='3'
+USBMODEM='1199:68a3'
+APN='data641003'
+APN_USER='0'
+APN_PASS='0'
+
 # Using Orange Senegal defaults:
-sudo /home/pi/umtskeeper/umtskeeper --sakisoperators "USBINTERFACE='3' OTHER='USBMODEM' USBMODEM='1199:68a3' APN='internet' APN_USER='internet' APN_PASS='0'" --sakisswitches "--sudo --console"
+sudo /home/pi/umtskeeper/umtskeeper --sakisoperators "USBINTERFACE='$USBINTERFACE' OTHER='USBMODEM' USBMODEM='$USBMODEM' APN='$APN' APN_USER='$APN_USER' APN_PASS='$APN_PASS'" --sakisswitches "--sudo --console"
 
--- run.sh
+++ run.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
 
-# Tip: You can press "Ctrl + a" twice to switch between windows
-
-# Starts a new detached screen session (-dm) named "pi" (-S) with the 
-# window title "3g" (-t), and runs 3g script
-screen -dmS pi -t 3g /home/pi/3g.sh
-
-# Connects to existing session "pi" (-S) and runs command (-X). 
-# When screen is run within screen, it creates a new window named "tunnel" (-t)
-# in the current session 
-screen -S pi -X screen -t tunnel /home/pi/tunnel.sh
-
-# Attach to 3g window
-screen -r pi -p 3g
-
+lxterminal -t '3g' -e /home/pi/remote-debugger/3g.sh &
+lxterminal -t 'tunnel' -e /home/pi/remote-debugger/tunnel.sh &
